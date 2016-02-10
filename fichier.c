3019c7dcb49534273cd1d566eeb44a9e9e190f36
Tosh Camille <tochecamille@gmail.com>
2016-02-10 09:27:38 +0100

    --,

--- fichier.c
+++ fichier.c
@@ -18,41 +18,46 @@ produit unProduit;
     int prix[] = {10, 50, 1, 3, 2, 2};
 */
     int r;
-    int i = 1;
+    int i = 0;
     FILE* ficheProduit = fopen( "Produit", "w+" );
 
     int combien;
     printf("Combien de produit ? ");
     scanf("%d", &combien);
 
-    while(i <= combien){
+    while(i <= combien - 1){
         printf("\n \t Tour %d", i);
-        printf("\nNum produit : ");
-        scanf("%d", unProduit.num_produit);
+        printf("\nNum : ");
+        scanf("%d", &unProduit.num_produit);
 
         printf("Lib : ");
         scanf("%s", unProduit.lib_produit);
 
         printf("Prix : ");
-        scanf("%d", unProduit.prix);
+        scanf("%d", &unProduit.prix);
 
         fwrite(&unProduit, sizeof(produit), 1, ficheProduit);
         i++;
     }
+
+    i = 0;
+
+    while(i <= combien - 1){
+        printf("\n \t Affichage du tour %d \n", i);
+        printf("Num : %d\n", unProduit.num_produit);
+        printf("Lib : %s\n", unProduit.lib_produit);
+        printf("Prix : %d\n", unProduit.num_produit);
+        i++;
+    }
     if (ficheProduit != 0) {
         r = 1;
-        printf("Le fichier est ecrit.\n");
+        printf("\n Le fichier est ecrit.\n");
     }
-
-    fread(&unProduit, sizeof(produit), 1, ficheProduit);
-    printf("Num : %d \n", unProduit.num_produit);
-    printf("Lib %s\n", unProduit.lib_produit);
-    printf("Prix %d\n", unProduit.num_produit);
-
     fclose(ficheProduit);
     /*
     int *numProduit[] = {5, 10, 12, 6, 7, 8};
     int *position[] = {1, 2, 3, 4, 5, 6};
     */
+
 return r;
 }
